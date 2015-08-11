ebb68d1fefe63b28cafa2ff3d516aed29e5a7b88
Jessica Joly & Alyssa Horrocks <student@epicodus.com>
2015-08-11 16:14:59 -0700

    Fifth spec pass

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -3,7 +3,7 @@ class Tamagotchi
     @name = name
     @food_level = 10
     @sleep_level = 10
-    @play_level = 10
+    @activity_level = 10
   end
 
   define_method(:name) do
@@ -18,18 +18,26 @@ class Tamagotchi
     @sleep_level
   end
 
-  define_method(:play_level) do
-    @play_level
+  define_method(:activity_level) do
+    @activity_level
   end
 
   define_method(:is_alive) do
-    if @food_level > 0
+    if @food_level > 0 && @sleep_level > 0 && @activity_level > 0
       true
     else
       false
     end
   end
 
+  define_method(:time_passes) do
+      @food_level = @food_level.-(3)
+      @sleep_level = @sleep_level.-(2)
+      @activity_level = @activity_level.-(1)
+  end
 
+  define_method(:feed) do
+    @food_level = 10
+  end
 
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -8,7 +8,7 @@ describe(Tamagotchi) do
         expect(my_pet.name()).to(eq("Gitty Hubby"))
         expect(my_pet.food_level()).to(eq(10))
         expect(my_pet.sleep_level()).to(eq(10))
-        expect(my_pet.play_level()).to(eq(10))
+        expect(my_pet.activity_level()).to(eq(10))
     end
   end
 
@@ -17,6 +17,29 @@ describe(Tamagotchi) do
       my_pet = Tamagotchi.new("Gitty Hubby")
       expect(my_pet.is_alive()).to(eq(true))
     end
+
+    it("is dead if food level is 0") do
+      my_pet = Tamagotchi.new("Gitty Hubby")
+      my_pet.food_level()
+      expect(my_pet.is_alive()).to(eq(false))
+    end
   end
 
+  describe("#time_passes") do
+      it("decreases the amount of food the Tamagotchi has left by 1") do
+      my_pet = Tamagotchi.new("Gitty Hubby")
+      my_pet.time_passes()
+      expect(my_pet.food_level()).to(eq(7))
+      expect(my_pet.sleep_level()).to(eq(8))
+      expect(my_pet.activity_level()).to(eq(9))
+    end
+  end
+
+  describe("#feed") do
+    it("replenish the Tamagotchi's food level to 10") do
+      my_pet = Tamagotchi.new("Git Hubby")
+      my_pet.feed()
+      expect(my_pet.feed()).to(eq(10))
+    end
+  end
 end
